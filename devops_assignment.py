494a69810acf90620335a838deac37ed018254b7
Divyansh-Garg113 <48668540+Divyansh-Garg113@users.noreply.github.com>
2022-09-18 14:35:50 +0530

    Update devops_assignment.py
    updating python file to test auto-deployment

--- devops_assignment.py
+++ devops_assignment.py
@@ -7,7 +7,7 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('cron', second='0')
 def say_hello():
-        print("test Github action ")
+        print("Github action test")
         print("Hello", datetime.datetime.utcnow())
 if __name__ == '__main__':
         procs = []
