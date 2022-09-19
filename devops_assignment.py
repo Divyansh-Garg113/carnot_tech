0c0a711779cf09519c388f1272618377662fdb01
Divyansh-Garg113 <48668540+Divyansh-Garg113@users.noreply.github.com>
2022-09-19 13:31:48 +0530

    Update devops_assignment.py

--- devops_assignment.py
+++ devops_assignment.py
@@ -7,7 +7,7 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('cron', second='0')
 def say_hello():
-        print("Github action test")
+        print("Github action tests")
         print("Hello", datetime.datetime.utcnow())
 if __name__ == '__main__':
         procs = []
